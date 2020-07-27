272451f7bf4a0a88bfb1921db51f8b6d664af747
Cristiancj99 <cristiancj1999@gmail.com>
2020-07-27 13:52:58 +0200

    Iniciación objetos

---
+++ Iniciacion objetos/Ejemplo.java
@@ -0,0 +1,19 @@
+
+package objetos;
+
+
+public class Ejemplo {
+    
+    String nombre;
+    String apellido;
+    
+   public void ImprimirEjemplo(){
+       
+       System.out.println("nombre = " + nombre);
+       System.out.println("apellido = " + apellido);
+       
+   }
+   
+}
+
+
---
+++ Iniciacion objetos/PruebaObjeto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author crist
+ */
+public class PruebaObjeto {
+    
+    public static void main(String[] args) {
+        
+        Ejemplo uno;
+        uno=new Ejemplo();
+        uno.nombre= "manolo";
+        uno.apellido= "Ruiz";
+        uno.ImprimirEjemplo();
+        
+    }
+    
+}
